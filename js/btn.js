58386b0353e2f02ba2ca990d8b8ba1c6ddd2cb32
Arkadiy2907 <arkadiy2907@gmail.com>
2023-09-02 00:29:53 +0300

    bug fix js

--- js/btn.js
+++ js/btn.js
@@ -1,26 +1,32 @@
 'use strict'
 
-const buttonContainer = document.getElementById('buttonContainer');
-const buttons = buttonContainer.querySelectorAll('.button');
-const image = document.querySelector('.image');
+const init = () => {
+  const buttonContainer = document.querySelector('.button_container');
+  const buttons = buttonContainer.querySelectorAll('.button');
+  const image = document.querySelector('.image');
 
-const handle = e => {
-  if (e.target.tagName === 'BUTTON') {
-    const button = e.target;
-    const name = button.dataset.name;
-    button.classList.toggle('open');
-    button.innerHTML = button.classList.contains('open') ? `<span>&#8722;&ensp;</span> ${name} ` : '<span>&#43;</span>';
+  const handle = e => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const name = button.dataset.name;
+      button.classList.toggle('open');
+      button.innerHTML = button.classList.contains('open')
+        ? `<b>&#8722;</b>&ensp;${name}`
+        : '';
+    }
   }
+
+  buttonContainer.addEventListener('click', handle);
+
+  image.addEventListener('click', () => {
+    buttons.forEach(button => {
+      if (button.classList.contains('open')) {
+        button.innerHTML = '';
+        button.classList.remove('open');
+      }
+    });
+    image.removeEventListener('click', () => { });
+  });
 }
 
-window.addEventListener('load', handle)
-
-buttonContainer.addEventListener('click', handle);
-
-image.addEventListener('click', () => {
-  buttons.forEach(button => {
-    button.innerHTML = '<span>&#43;</span>';
-    button.classList.remove('open');
-  });
-  image.removeEventListener('click', () => { });
-});
+window.addEventListener('load', init);
