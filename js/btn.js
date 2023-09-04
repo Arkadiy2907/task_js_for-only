c6b9a1f2005fd518957c3df79987e6686923bd60
Arkadiy2907 <arkadiy2907@gmail.com>
2023-09-04 20:37:34 +0300

    bug fix

--- js/btn.js
+++ js/btn.js
@@ -2,30 +2,23 @@
 
 const init = () => {
   const buttonContainer = document.querySelector('.button_container');
-  const buttons = buttonContainer.querySelectorAll('.button');
   const image = document.querySelector('.image');
 
   const handle = e => {
     if (e.target.tagName === 'BUTTON') {
       const button = e.target;
-      const name = button.dataset.name;
       button.classList.toggle('open');
-      button.innerHTML = button.classList.contains('open')
-        ? `<b>&#8722;</b>&ensp;${name}`
-        : '';
     }
   }
 
   buttonContainer.addEventListener('click', handle);
 
   image.addEventListener('click', () => {
-    buttons.forEach(button => {
-      if (button.classList.contains('open')) {
-        button.innerHTML = '';
-        button.classList.remove('open');
-      }
-    });
-    image.removeEventListener('click', () => { });
+    const openButton = buttonContainer.querySelector('.button.open');
+    if (openButton) {
+      openButton.classList.remove('open');
+      openButton.innerHTML = '';
+    }
   });
 }
 
